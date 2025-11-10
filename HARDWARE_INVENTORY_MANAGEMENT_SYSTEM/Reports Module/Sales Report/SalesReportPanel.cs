7a2f4ffb32e595f21c87a1133511b497ab8cf2dc
achi <achi@ACHILLES>
2025-11-10 22:55:46 +0800

    sales report pagination fix

--- HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Reports Module/Sales Report/SalesReportPanel.cs
+++ HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Reports Module/Sales Report/SalesReportPanel.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module.Inventory_Report;
+using HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module.Sales_Report;
 
 namespace HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module
 {
@@ -15,6 +17,50 @@ namespace HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module
         public SalesPage()
         {
             InitializeComponent();
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+            //page 1
+            Showpage1Sales();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            //page 2
+            Showpage2Sales();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            //page 3
+            Showpage3Sales();
+        }
+        private void Showpage1Sales()
+        {
+            panel1.Controls.Clear();
+            var page1Sales = new SalesPage1();
+            page1Sales.Dock = DockStyle.Fill;
+            panel1.Controls.Add(page1Sales);
+        }
+        private void Showpage2Sales()
+        {
+            panel1.Controls.Clear();
+            var page2Sales = new SalesPage2();
+            page2Sales.Dock = DockStyle.Fill;
+            panel1.Controls.Add(page2Sales);
+        }
+        private void Showpage3Sales()
+        {
+            panel1.Controls.Clear();
+            var page3Sales = new SalesPage3();
+            page3Sales.Dock = DockStyle.Fill;
+            panel1.Controls.Add(page3Sales);
+        }
+        
+        private void SalesReportPanel_load(object sender, EventArgs e)
+        {
+            Showpage1Sales();
         }
     }
 }
