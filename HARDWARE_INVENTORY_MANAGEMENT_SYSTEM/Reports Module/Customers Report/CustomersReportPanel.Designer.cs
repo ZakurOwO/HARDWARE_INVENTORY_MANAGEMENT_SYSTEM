8d7b3c4b7f2d9a93f6b5a6d837f61b6fdd35f973
achi <achi@ACHILLES>
2025-11-12 02:00:36 +0800

    customers pagination fix

--- HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Reports Module/Customers Report/CustomersReportPanel.Designer.cs
+++ HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Reports Module/Customers Report/CustomersReportPanel.Designer.cs
@@ -42,43 +42,48 @@
             // inventoryFilter_Button1
             // 
             this.inventoryFilter_Button1.BackColor = System.Drawing.Color.Transparent;
-            this.inventoryFilter_Button1.Location = new System.Drawing.Point(661, 11);
+            this.inventoryFilter_Button1.Location = new System.Drawing.Point(881, 14);
+            this.inventoryFilter_Button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.inventoryFilter_Button1.Name = "inventoryFilter_Button1";
-            this.inventoryFilter_Button1.Size = new System.Drawing.Size(102, 43);
+            this.inventoryFilter_Button1.Size = new System.Drawing.Size(136, 53);
             this.inventoryFilter_Button1.TabIndex = 14;
             // 
             // mainButton1
             // 
             this.mainButton1.ButtonName = "Generate Report";
-            this.mainButton1.Location = new System.Drawing.Point(764, 11);
+            this.mainButton1.Location = new System.Drawing.Point(1019, 14);
+            this.mainButton1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.mainButton1.Name = "mainButton1";
-            this.mainButton1.Size = new System.Drawing.Size(149, 44);
+            this.mainButton1.Size = new System.Drawing.Size(199, 54);
             this.mainButton1.TabIndex = 13;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Lexend SemiBold", 11.25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 24);
+            this.label1.Size = new System.Drawing.Size(177, 24);
             this.label1.TabIndex = 11;
             this.label1.Text = "Customer Reports";
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(18, 58);
+            this.panel1.Location = new System.Drawing.Point(24, 71);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(905, 470);
+            this.panel1.Size = new System.Drawing.Size(1207, 578);
             this.panel1.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Lexend Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(386, 543);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(515, 668);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(153, 17);
+            this.label2.Size = new System.Drawing.Size(185, 17);
             this.label2.TabIndex = 28;
             this.label2.Text = "Showing 1 out of 40 records";
             // 
@@ -92,13 +97,15 @@
             this.guna2Button5.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.guna2Button5.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.guna2Button5.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button5.Font = new System.Drawing.Font("Lexend Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(28)))), ((int)(((byte)(35)))));
-            this.guna2Button5.Location = new System.Drawing.Point(839, 535);
+            this.guna2Button5.Location = new System.Drawing.Point(1119, 658);
+            this.guna2Button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Button5.Name = "guna2Button5";
-            this.guna2Button5.Size = new System.Drawing.Size(29, 29);
+            this.guna2Button5.Size = new System.Drawing.Size(39, 36);
             this.guna2Button5.TabIndex = 27;
             this.guna2Button5.Text = "1";
+            this.guna2Button5.Click += new System.EventHandler(this.guna2Button5_Click);
             // 
             // guna2Button6
             // 
@@ -112,11 +119,12 @@
             this.guna2Button6.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.guna2Button6.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.guna2Button6.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button6.Font = new System.Drawing.Font("Lexend Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(28)))), ((int)(((byte)(35)))));
-            this.guna2Button6.Location = new System.Drawing.Point(811, 535);
+            this.guna2Button6.Location = new System.Drawing.Point(1081, 658);
+            this.guna2Button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Button6.Name = "guna2Button6";
-            this.guna2Button6.Size = new System.Drawing.Size(29, 29);
+            this.guna2Button6.Size = new System.Drawing.Size(39, 36);
             this.guna2Button6.TabIndex = 26;
             // 
             // guna2Button4
@@ -131,11 +139,12 @@
             this.guna2Button4.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.guna2Button4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.guna2Button4.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button4.Font = new System.Drawing.Font("Lexend Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(28)))), ((int)(((byte)(35)))));
-            this.guna2Button4.Location = new System.Drawing.Point(895, 535);
+            this.guna2Button4.Location = new System.Drawing.Point(1193, 658);
+            this.guna2Button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Button4.Name = "guna2Button4";
-            this.guna2Button4.Size = new System.Drawing.Size(29, 29);
+            this.guna2Button4.Size = new System.Drawing.Size(39, 36);
             this.guna2Button4.TabIndex = 25;
             // 
             // guna2Button2
@@ -148,17 +157,19 @@
             this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.guna2Button2.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button2.Font = new System.Drawing.Font("Lexend Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(28)))), ((int)(((byte)(35)))));
-            this.guna2Button2.Location = new System.Drawing.Point(867, 535);
+            this.guna2Button2.Location = new System.Drawing.Point(1156, 658);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Button2.Name = "guna2Button2";
-            this.guna2Button2.Size = new System.Drawing.Size(29, 29);
+            this.guna2Button2.Size = new System.Drawing.Size(39, 36);
             this.guna2Button2.TabIndex = 24;
             this.guna2Button2.Text = "2";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
             // 
             // CustomersReportPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.label2);
@@ -170,8 +181,9 @@
             this.Controls.Add(this.inventoryFilter_Button1);
             this.Controls.Add(this.mainButton1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "CustomersReportPanel";
-            this.Size = new System.Drawing.Size(937, 570);
+            this.Size = new System.Drawing.Size(1249, 702);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Reports Module/Customers Report/CustomersReportPanel.cs
+++ HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Reports Module/Customers Report/CustomersReportPanel.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module.Customers_Report;
+using HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module.Sales_Report;
 
 namespace HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module
 {
@@ -15,6 +17,38 @@ namespace HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Reports_Module
         public CustomersReportPanel()
         {
             InitializeComponent();
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+            //Page 1
+            Showpage1Customers();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            //Page 2
+            Showpage2Customers();
+        }
+
+        private void Showpage1Customers()
+        {
+            panel1.Controls.Clear();
+            var page1Customers = new CustomersPage1();
+            page1Customers.Dock = DockStyle.Fill;
+            panel1.Controls.Add(page1Customers);
+        }
+        private void Showpage2Customers()
+        {
+            panel1.Controls.Clear();
+            var page2Customers = new CustomersPage2();
+            page2Customers.Dock = DockStyle.Fill;
+            panel1.Controls.Add(page2Customers);
+        }
+
+        private void CustomersReportPanel_load(object sender, EventArgs e)
+        {
+            Showpage1Customers();
         }
     }
 }
